8c69c1863f28a657ae9e38b2e89053db84b929bc
Ertuğrul Erata <ertugrulerata@gmail.com>
2019-08-16 01:10:24 +0300

    baselayout build system-release link has a problem.

---
+++ system/base/baselayout/actions.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+
+def build():
+    # NOTE: This is only for the start-stop-daemon
+    autotools.make('-C src CC="%s" LD="%s %s" CFLAGS="%s"' % (get.CC(), get.CC(), get.LDFLAGS(), get.CFLAGS()))
+
+def install():
+    def chmod(path, mode):
+        shelltools.chmod("%s%s" % (get.installDIR(), path), mode)
+
+    # Install everything
+    pisitools.insinto("/", "root/*")
+    
+    # Install baselayout utilities
+    shelltools.cd("src/")
+    autotools.rawInstall('DESTDIR="%s"' % get.installDIR())
+  
+    # create needed directories
+    pisitools.dodir("/lib")
+    pisitools.dodir("/tmp")
+    pisitools.dodir("/var/tmp")
+    pisitools.dodir("/run/shm")
+    pisitools.dodir("/run")
+    pisitools.dodir("/run/lock")
+
+    # Adjust permissions
+    chmod("/tmp", 0o1777)
+    chmod("/var/tmp", 0o1777)
+    chmod("/run/shm", 0o1777)
+    chmod("/var/lock", 0o775)
+    chmod("/usr/share/baselayout/shadow", 0o600)
+
+    if get.ARCH() == "x86_64":
+        # Directories for 32bit libraries
+        pisitools.dodir("/lib32")
+        pisitools.dodir("/usr/lib32")
+
+        # Hack for binary blobs built on multi-lib systems
+        pisitools.dosym("lib", "/lib64")
+
+    #pisitools.dosym("pisilinux-release", "/etc/system-release")
