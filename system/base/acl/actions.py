241b9f3900d2562d4830dcefadb11be46f665418
Ertuğrul Erata <ertugrulerata@gmail.com>
2019-08-16 00:09:30 +0300

    attr and acl build

---
+++ system/base/acl/actions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.export("OPTIMIZER", get.CFLAGS())
+    shelltools.export("DEBUG", "-DNDEBUG")
+
+    autotools.rawConfigure("--libdir=/lib \
+                            --disable-static  \
+                            --mandir=/usr/share/man \
+                            --libexecdir=/lib \
+                            --bindir=/bin")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    shelltools.chmod("%s/lib/libacl.so.*.*.*" % get.installDIR(), 0o755)
+    shelltools.copytree("%s/lib/pkgconfig" % get.installDIR(), "%s/usr/lib/pkgconfig" % get.installDIR())
+    pisitools.removeDir("/lib/pkgconfig")
+
+    pisitools.dodoc("README")
