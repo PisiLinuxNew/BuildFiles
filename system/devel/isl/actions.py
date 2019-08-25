4df99198207d687c98e706f200d738e5c64f40de
Ertuğrul Erata <ertugrulerata@gmail.com>
2019-08-25 15:59:19 +0300

    isl update

---
+++ system/devel/isl/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def check():
+    autotools.make("check")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    pisitools.domove("/usr/lib/libisl.so.21.0.0-gdb.py", "/usr/share/gdb/auto-load")
+
+    pisitools.dodoc("AUTHORS","ChangeLog","doc/manual.pdf")
